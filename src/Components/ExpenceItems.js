ff3a03465abac90ab06c623d9b97aa8fda99e691
kapil11Agnihotri <kapil90444@gmail.com>
2022-10-22 21:57:13 +0530

    new

---
+++ src/Components/ExpenceItems.js
@@ -0,0 +1,29 @@
+import './ExpenceItems.css';
+
+function ExpenceItems(){
+    return (
+       <div>
+        <div>
+            October 22th 2022
+        </div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$265</div>
+        </div>
+        <div>
+            <h2>Food</h2>
+            <div>$10</div>
+        </div>
+        <div>
+            <h2>Petrol</h2>
+            <div>$100</div>
+        </div>
+        <div>
+            <h2>Movies</h2>
+            <div>$200</div>
+        </div>
+       </div>
+   
+    )
+}
+export default ExpenceItems;
