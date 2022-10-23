c60c7714de3d4d8de01f9124a59311a46099dedd
kapil11Agnihotri <kapil90444@gmail.com>
2022-10-23 16:37:41 +0530

    Arrow function impelemented

---
+++ src/Components/Expense.js
@@ -0,0 +1,37 @@
+import ExpenceItems from "./ExpenceItems";
+import "./Expense.css";
+
+const Expense=(props)=> {
+  
+  return (
+    
+    <div className="expenses">
+        <ExpenceItems
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+          locationOfExpenditure={props.items[0].locationOfExpenditure}
+        />
+        <ExpenceItems
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+          locationOfExpenditure={props.items[1].locationOfExpenditure}
+        />
+        <ExpenceItems
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+          locationOfExpenditure={props.items[2].locationOfExpenditure}
+        />
+        <ExpenceItems
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+          locationOfExpenditure={props.items[3].locationOfExpenditure}
+        />
+    </div>
+   
+  );
+}
+export default Expense;
