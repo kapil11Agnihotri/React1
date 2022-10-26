76291160a430ed6573c67e3b85860441fdc1f7b8
kapil11Agnihotri <kapil90444@gmail.com>
2022-10-26 21:57:29 +0530

    filter is working

--- src/Components/Expense/Expense.js
+++ src/Components/Expense/Expense.js
@@ -10,25 +10,32 @@ const Expense = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <>
-    <Card>
-      <div className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {props.items.map((expense) => (
-          <ExpenceItems
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
+      <Card>
+        <div className="expenses">
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
           />
-        ))}
-      </div>
-    </Card>
+          {filteredExpenses.length === 0 ? (
+            <p>No Expenses Found</p>
+          ) : (
+            filteredExpenses.map((expense) => (
+              <ExpenceItems
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+            ))
+          )}
+        </div>
+      </Card>
     </>
   );
 };
